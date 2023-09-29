b70ca524b60841e16e51c606c8c6b94dffdaa5df
sambhavm22 <58766591+sambhavm22@users.noreply.github.com>
2023-09-29 11:39:46 +0200

    data ingestion

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline, STAGE_NAME
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f" >>>>>>> stage {STAGE_NAME} completed <<<<< \n\nx========x")
+    except Exception as e:
+        logger.exception(e)
+
